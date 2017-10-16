a3feb2b79d53be1524222ecd96ee28be731518e0
vsoedhwa <vsoedhwa@sfu.ca>
2017-10-16 13:18:33 -0700

    first commit archiving

---
+++ Source/Gifmakercode.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace gifmakercode
+{   
+    public class GifMaker : IDisposable
+    {
+        #region Header Constants
+        private const string FileType = "GIF";
+        private const string FileVersion = "89a";
+        private const byte FileTrailer = 0x3b;
+
+        private const int Extension_Introducer = 0xff21;
+        private const byte Size_of_Extension_Block = 0x0b;
+        private const string ApplicationIdentification = "NETSCAPE2.0";
+
+        private const int Graphic_Control_Label = 0xf921;
+        private const byte Size_of_remaining_fields = 0x04;
+
+        private const long SourceGlobalColorInfoPosition = 10;
+        private const long SourceGraphicControlExtensionPosition = 781;
+        private const long SourceGraphicControlExtensionLength = 8;
+        private const long SourceImageBlockPosition = 789;
+        private const long SourceImageBlockHeaderLength = 11;
+        private const long SourceColorBlockPosition = 13;
+        private const long SourceColorBlockLength = 768;
+
+        private const byte Block_Terminator = 0;
+        #endregion
+
+        private bool _GifHeadconfirm = true;                                      //first image
+        private int? _width;
+        private int? _height;
+        private int? _repeatCount;
+        private readonly Stream _stream;
+                
+        public TimeSpan FrameIntervalTime { get; set; }
+               
+        public GifMaker(Stream stream, int? width = null, int? height = null, int? repeatCount = null)
+        {
+            _stream = stream;
+            _width = width;
+            _height = height;
+            _repeatCount = repeatCount;
+        }
+
+        public void AddFrame(Image img, int x = 0, int y = 0, TimeSpan? frameDelay = null)
+        {
+            using (var gifStream = new MemoryStream())
+            {
+                img.Save(gifStream, ImageFormat.Gif);
+                if (_GifHeadconfirm)                                            // get the global color table info
+                {
+                    InitHeader(gifStream, img.Width, img.Height);
+                }
+                WriteGraphicControlBlock(gifStream, frameDelay.GetValueOrDefault(FrameIntervalTime));
+                WriteImgBlock(gifStream, !_GifHeadconfirm, x, y, img.Width, img.Height);
+            }
+            _GifHeadconfirm = false;
+        }
+
+        private void InitHeader(Stream sourceGif, int w, int h)
+        {
+            // File Header
+            Write_String(FileType);
+            Write_String(FileVersion);
+            Write_Short(_width.GetValueOrDefault(w));                           // Initial Logical Width
+            Write_Short(_height.GetValueOrDefault(h));                          // Initial Logical Height
+            sourceGif.Position = SourceGlobalColorInfoPosition;
+            WriteByt(sourceGif.ReadByte());                                     // Global Color Table Info
+            WriteByt(0);                                                        // Background Color Index
+            WriteByt(0);                                                        // Pixel aspect ratio
+            WriteColorTable(sourceGif);
+
+            // App Extension Header
+            Write_Short(Extension_Introducer);
+            WriteByt(Size_of_Extension_Block);
+            Write_String(ApplicationIdentification);
+            WriteByt(3);                                                        // Application block length
+            WriteByt(1);
+            Write_Short(_repeatCount.GetValueOrDefault(0));                     // Repeat count for images.
+            WriteByt(0);                                                        // terminator
+        }
+
+        private void WriteColorTable(Stream sourceGif)
+        {
+            sourceGif.Position = SourceColorBlockPosition;                      // Locating the image color table
+            var colorTable = new byte[SourceColorBlockLength];
+            sourceGif.Read(colorTable, 0, colorTable.Length);
+            _stream.Write(colorTable, 0, colorTable.Length);
+        }
+
+        private void WriteGraphicControlBlock(Stream sourceGif, TimeSpan frameDelay)
+        {
+            sourceGif.Position = SourceGraphicControlExtensionPosition;         // Locating the source GCE
+            var blockhead = new byte[SourceGraphicControlExtensionLength];
+            sourceGif.Read(blockhead, 0, blockhead.Length);                     // Reading source GCE
+
+            Write_Short(Graphic_Control_Label);                                 // Identifier
+            WriteByt(Size_of_remaining_fields);                                 // Block Size
+            WriteByt(blockhead[3] & 0xf7 | 0x08);                               // Setting disposal flag
+            Write_Short(Convert.ToInt32(frameDelay.TotalMilliseconds / 10));    // Setting frame delay
+            WriteByt(blockhead[6]);                                             // Transparent color index
+            WriteByt(Block_Terminator);                                         // Terminator
+        }
+
+        private void WriteImgBlock(Stream sourceGif, bool includeColorTable, int x, int y, int h, int w)
+        {
+            sourceGif.Position = SourceImageBlockPosition;                      // Locating the image block
+            var header = new byte[SourceImageBlockHeaderLength];
+            sourceGif.Read(header, 0, header.Length);
+            WriteByt(header[0]);                                                // Separator
+            Write_Short(x);                                                     // Position X
+            Write_Short(y);                                                     // Position Y
+            Write_Short(h);                                                     // Height
+            Write_Short(w);                                                     // Width
+
+            if (includeColorTable)                                              // If first frame, use global color table - else use local
+            {
+                sourceGif.Position = SourceGlobalColorInfoPosition;
+                WriteByt(sourceGif.ReadByte() & 0x3f | 0x80);                   // Enabling local color table
+                WriteColorTable(sourceGif);
+            }
+            else
+            {
+                WriteByt(header[9] & 0x07 | 0x07);                              // Disabling local color table
+            }
+
+            WriteByt(header[10]);                                               // LZW Min Code Size
+
+            // Read/Write image data
+            sourceGif.Position = SourceImageBlockPosition + SourceImageBlockHeaderLength;
+
+            var dataLength = sourceGif.ReadByte();
+            while (dataLength > 0)
+            {
+                var imgData = new byte[dataLength];
+                sourceGif.Read(imgData, 0, dataLength);
+
+                _stream.WriteByte(Convert.ToByte(dataLength));
+                _stream.Write(imgData, 0, dataLength);
+                dataLength = sourceGif.ReadByte();
+            }
+
+            _stream.WriteByte(Block_Terminator);                                // Terminator
+
+        }
+
+        private void WriteByt(int value)
+        {
+            _stream.WriteByte(Convert.ToByte(value));
+        }
+
+        private void Write_Short(int value)
+        {
+            _stream.WriteByte(Convert.ToByte(value & 0xff));
+            _stream.WriteByte(Convert.ToByte((value >> 8) & 0xff));
+        }
+
+        private void Write_String(string value)
+        {
+            _stream.Write(value.ToArray().Select(c => (byte)c).ToArray(), 0, value.Length);
+        }
+
+        public void Dispose()
+        {
+            // Complete File
+            WriteByt(FileTrailer);
+
+            // Pushing data
+            _stream.Flush();
+        }
+    }
+}
---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Windows.Media;
+using System;
+using System.Windows.Interop;
+using AForge.Video.FFMPEG;
+using System.IO;
+using System.Windows.Media.Animation;
+using System.Drawing.Imaging;
+using System.Windows.Controls;
+using gifmakercode;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+    {
+
+        OpenFileDialog openFileDialog1;
+
+        long framecount = 0;
+        int framesskipped = 0;
+
+        bool horizontalflip = false;
+        bool verticalflip   = false;
+        int qualitylevel    = 50;
+        
+        System.Windows.Media.Imaging.Rotation rotationselected = Rotation.Rotate0;
+                
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Converttogif_btn.IsEnabled = false;
+            gifcoverage_slider.IsEnabled = false;
+            slider.IsEnabled = false;
+        }
+
+        private void openfile_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Video Files(*.AVI;*.MPEG;*.MPG;*.MP4)|*.AVI;*.MPEG;*.MPG;*.MP4|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select a video File";
+            
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                MediaTimeline tl = new MediaTimeline(new Uri(openFileDialog1.FileName.ToString()));
+                
+                mediaElement.Clock = tl.CreateClock(true) as MediaClock;
+
+                mediaElement.MediaOpened += (o, ef) =>
+                {
+                    slider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+                    mediaElement.Clock.Controller.Pause();
+                };
+
+                slider.ValueChanged += (o, ef) =>
+                {
+                    mediaElement.Clock.Controller.Seek(TimeSpan.FromSeconds(slider.Value), TimeSeekOrigin.BeginTime);
+                };
+                
+                VideoFileReader videoreader;
+                videoreader = new VideoFileReader();
+                // open video file
+                videoreader.Open(openFileDialog1.FileName.ToString());
+                framecount = videoreader.FrameCount;
+                textBox.Text = framecount.ToString();
+                videoreader.Close();
+
+                slider.IsEnabled = true;
+                gifcoverage_slider.IsEnabled = true;
+                Converttogif_btn.IsEnabled = true;
+
+                calculateframeskip(20); // 20 % frames
+            }
+        }
+
+        private void Converttogif_btn_click(object sender, RoutedEventArgs e)
+        {
+            VideoFileReader videoreader = new VideoFileReader();
+            // open video file
+            videoreader.Open(openFileDialog1.FileName.ToString());
+                       
+
+            GifBitmapEncoder thegifencoder = new GifBitmapEncoder();
+
+            string filename = "thegiffile.gif";
+
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            using (var gif = File.OpenWrite("thegiffile.gif"))
+            using (var encoder = new GifMaker(gif))           // customlibs
+                                           
+
+            for (int i = 0; i < framecount; i++)
+            {
+                Bitmap videoFrame = videoreader.ReadVideoFrame();
+                //insert jpeg commpression here. 
+
+                BitmapSource imagesrc = Converttobmpscr(videoFrame);
+
+                MemoryStream ms = new MemoryStream(); //saving to memory instead of file
+
+                JpegBitmapEncoder jpegencoder = new JpegBitmapEncoder();
+                jpegencoder.FlipHorizontal = horizontalflip;
+                jpegencoder.FlipVertical = verticalflip;
+                jpegencoder.QualityLevel = qualitylevel;
+                jpegencoder.Rotation = rotationselected;
+                jpegencoder.Frames.Add(BitmapFrame.Create(imagesrc));
+                jpegencoder.Save(ms);
+
+                System.Drawing.Image compr_img = System.Drawing.Image.FromStream(ms);
+
+                //Bitmap compr_frame = new Bitmap(compr_img);
+                
+                if (i % framesskipped == 0)
+                {
+                   //IntPtr bmp = compr_frame.GetHbitmap();
+                   //BitmapSource src = Imaging.CreateBitmapSourceFromHBitmap(
+                   //bmp,
+                   //IntPtr.Zero,
+                   //Int32Rect.Empty,
+                   //BitmapSizeOptions.FromEmptyOptions());
+                   //thegifencoder.Frames.Add(BitmapFrame.Create(src));
+
+                    encoder.AddFrame(compr_img);
+
+                }
+
+                videoFrame.Dispose();
+                jpegencoder = null;
+            }
+            
+            videoreader.Close();
+
+            //using (FileStream fs = new FileStream("thegiffile.gif", FileMode.Create))            // windows libs
+            //{
+            //    thegifencoder.Save(fs);
+            //}
+
+            thegifencoder = null;           
+
+        }
+
+        private void gifcoverage_slider_changedVal(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            calculateframeskip((int)e.NewValue);
+        }
+
+
+        public void calculateframeskip(int e)
+        {
+            decimal gifcoverage = 1;
+            int gifcoverageroof = 0;
+            int percentage = e;
+
+            try
+            {
+                percentageBox.Text = percentage.ToString();
+                gifcoverage = (decimal)(framecount / (framecount * ((double)percentage / 100)));
+                gifcoverageroof = Convert.ToInt32(Math.Round(gifcoverage));
+
+                f_skipBox.Text = gifcoverageroof.ToString();
+            }
+            catch
+            {
+
+            }
+
+            framesskipped = gifcoverageroof;
+        }
+
+        public BitmapSource Converttobmpscr(Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height, 96, 96, PixelFormats.Bgr24, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+            bitmap.UnlockBits(bitmapData);
+
+            return bitmapSource;
+        }
+
+        private void quality_slider_changedVal(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            getslidervalues((int)e.NewValue);
+        }
+
+        public void getslidervalues(int e)
+        {
+            try
+            {
+                quality_percentage.Text = e.ToString();
+                qualitylevel = e;
+            }
+            catch
+            {
+
+            }           
+        }
+
+        private void RadioButtonChecked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (radioButton == null)
+                return;           
+
+            try
+            {
+                switch (radioButton.Name.ToString())
+                {
+                    case "rot_none":
+                        rotationselected = Rotation.Rotate0;
+                        break;
+                    case "rot_90":
+                        rotationselected = Rotation.Rotate90;
+                        break;
+                    case "rot_180":
+                        rotationselected = Rotation.Rotate180;
+                        break;
+                    case "rot_270":
+                        rotationselected = Rotation.Rotate270;
+                        break;
+                    default:
+                        rotationselected = Rotation.Rotate0;
+                        break;
+                }
+            }
+            catch
+            {
+
+            }            
+        }
+
+        private void horflip_enable(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            if (checkbox == null)
+                return;
+
+            if(checkbox.IsChecked == true)
+            {
+                horizontalflip = true;
+            }
+            else
+            {
+                horizontalflip = false;
+            }
+        }
+
+        private void vertflip_enable(object sender, RoutedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            if (checkbox == null)
+                return;
+
+            if (checkbox.IsChecked == true)
+            {
+                verticalflip = true;
+            }
+            else
+            {
+                verticalflip = false;
+            }
+        }
+    }
+}
+
+
+
+
+
